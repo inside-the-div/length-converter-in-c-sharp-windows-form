ccfbfbc0d1c0de215210ee0a9304921c669ea5b4
kamrul_hasan_10 <rkamrulhasan7@gmail.com>
2023-01-21 15:23:59 +0600

    Update database

--- Database/LengthHistoryDB.sql
+++ Database/LengthHistoryDB.sql
@@ -6,7 +6,7 @@ CREATE TABLE length_convert_history (
 	converted_id INT IDENTITY(1,1) PRIMARY KEY,
 	converted_from VARCHAR(20) NOT NULL,
 	converted_to VARCHAR(20) NOT NULL,
-	converted_number INT NOT NULL,
+	converted_number DECIMAL(18,4) NOT NULL,
 	result VARCHAR(50) NOT NULL,
 	converted_datetime DATETIME NOT NULL
 )
